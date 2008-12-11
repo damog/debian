16e16df67252091cb58747a081f94fe7cc0735b6
David Moreno <david@axiombox.com>
2008-12-11 15:42:15 -0500

    initial commit

---
+++ libxml-treepp-perl-0.33/make-dist.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+die () {
+    echo "$*" >&2
+    exit 1
+}
+doit () {
+    echo "\$ $*" >&2
+    $* || die "[ERROR:$?]"
+}
+
+rdf=t/example/index.rdf
+doit wget -O $rdf~ http://www.kawa.net/rss/index-e.rdf
+diff $rdf $rdf~ > /dev/null || doit /bin/mv -f $rdf~ $rdf
+/bin/rm -f $rdf~
+
+egrep -v '^t/.*\.t$' MANIFEST > MANIFEST~
+ls t/*.t >> MANIFEST~
+diff MANIFEST MANIFEST~ > /dev/null || doit /bin/mv -f MANIFEST~ MANIFEST
+/bin/rm -f MANIFEST~
+
+[ -f Makefile ] && doit make clean
+doit perl Makefile.PL
+doit make
+doit make disttest
+
+main=`grep 'lib/.*pm$' < MANIFEST | head -1`
+[ "$main" == "" ] && die "main module is not found in MANIFEST"
+doit pod2text $main > README~
+diff README README~ > /dev/null || doit /bin/mv -f README~ README
+/bin/rm -f README~
+
+doit make dist
+[ -d blib ] && doit /bin/rm -fr blib
+[ -f pm_to_blib ] && doit /bin/rm -f pm_to_blib
+[ -f Makefile.old ] && doit /bin/rm -f Makefile.old
+
+ls -lt *.tar.gz | head -1
---
+++ libxml-treepp-perl_0.33-1.1.dsc
@@ -0,0 +1,23 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 1.0
+Source: libxml-treepp-perl
+Binary: libxml-treepp-perl
+Architecture: all
+Version: 0.33-1.1
+Maintainer: David Moreno Garza <damog@debian.org>
+Standards-Version: 3.7.2
+Build-Depends: debhelper (>= 5.0.0)
+Build-Depends-Indep: perl (>= 5.8.8-7)
+Files: 
+ 62af6ba2b5a556d76f28b73e206693b2 55322 libxml-treepp-perl_0.33.orig.tar.gz
+ e9b55171ab3d68611ee419e0b08a669d 2108 libxml-treepp-perl_0.33-1.1.diff.gz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.6 (GNU/Linux)
+
+iD8DBQFH9+KtNIW6CNDsByMRAsAeAKCcSXjR+IcBkPuIFcVi30FtSEXCtQCg2I+Q
+aQoje9x3jGiRQA9SutUATGY=
+=dmrx
+-----END PGP SIGNATURE-----
